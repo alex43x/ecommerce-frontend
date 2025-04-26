afc18dd187bb4c65479837d8efc8f38895704a81
Alex <alexgllamosas@gmail.com>
2025-04-26 17:48:46 -0300

    Product Tab Refactorization

--- src/pages/dashboard/Products.jsx
+++ src/pages/dashboard/Products.jsx
@@ -1,19 +1,54 @@
-import React,{useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import { useProduct } from "../../context/product/ProductContext";
+import ProductTable from "../../components/dashboard/ProductTable";
+import ProductFormModal from "../../components/dashboard/ProductFormModal";
+import NewProductForm from "../../components/dashboard/NewProductForm";
 
 export default function Products() {
-  const { getProducts, products, loading } = useProduct();
+  const {
+    getProducts,
+    updateProduct,
+    deleteProduct,
+    products,
+    categories,
+    loading,
+    getCategories,
+  } = useProduct();
+
+  const [editProductId, setEditProductId] = useState(null);
+  const [editForm, setEditForm] = useState({});
+  const [addProduct, setAddProduct] = useState(false);
+
   useEffect(() => {
-    getProducts(1, 10);
+    getProducts({ page: 1, limit: 10 });
+    getCategories();
   }, []);
 
-  return(
+  return (
     <div>
-        {loading ? (
-        <p>Cargando...</p>
-      ) : (
-        products.map((product) => <p>{product.name} {product.price} </p>)
-      )}
+      <button onClick={() => setAddProduct(true)}>Agregar producto</button>
+
+      <ProductFormModal
+        isOpen={addProduct}
+        onClose={() => setAddProduct(false)}
+      >
+        <NewProductForm onExit={() => setAddProduct(false)} />
+      </ProductFormModal>
+
+      <ProductTable
+        products={products}
+        loading={loading}
+        categories={categories}
+        editProductId={editProductId}
+        setEditProductId={setEditProductId}
+        editForm={editForm}
+        setEditForm={setEditForm}
+        updateProduct={updateProduct}
+        deleteProduct={deleteProduct}
+        refreshProducts={() =>
+          getProducts({ page: 1, limit: 10, forceRefresh: true })
+        }
+      />
     </div>
-  )
+  );
 }
