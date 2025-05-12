7dd80f2fa1808ed7b0124baf1d8618fa9a740290
Alex <alexgllamosas@gmail.com>
2025-05-12 17:57:29 -0300

    Reports Tab

--- src/pages/dashboard/Reports.jsx
+++ src/pages/dashboard/Reports.jsx
@@ -2,18 +2,5 @@ import React,{useEffect} from "react";
 import { useProduct } from "../../context/product/ProductContext";
 
 export default function Reports() {
-  const { getProducts, products, loading } = useProduct();
-  useEffect(() => {
-    getProducts(1, 10);
-  }, []);
-
-  return(
-    <div>
-        {loading ? (
-        <p>Cargando...</p>
-      ) : (
-        products.map((product) => <p>{product.name} {product.price} </p>)
-      )}
-    </div>
-  )
+  return(<></>)
 }
