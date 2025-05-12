2184c34a3881632f35b2d8fb9a3163c063c4df1d
Alex <alexgllamosas@gmail.com>
2025-05-12 17:58:47 -0300

    Sale Provider (updateStatus function)

--- src/context/sale/SaleProvider.jsx
+++ src/context/sale/SaleProvider.jsx
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
 import { SaleContext } from "./SaleContext";
-
 export const SaleProvider = ({ children }) => {
   const [sales, setSales] = useState([]);
   const [page, setPage] = useState(1);
   const [sale, setSale] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [fetched,setFetched]=useState(false)
+  const [fetched, setFetched] = useState(false);
   const token = localStorage.getItem("AuthToken");
 
-  const getSales = async (page = 1, limit = 10, user = null) => {
-    if (fetched) return;
-
+  const getSales = async (
+    page = 1,
+    limit = 10,
+    user = null,
+    forceRefresh = false
+  ) => {
+    if (fetched && !forceRefresh) return;
+    setLoading(true);
     const queryparams = {
       page,
       limit,
+      status: "pending",
       ...(user && { user }),
     };
+
     const params = new URLSearchParams(queryparams);
     try {
       const res = await fetch(
@@ -33,7 +39,7 @@ export const SaleProvider = ({ children }) => {
       console.log(data.sales);
       setSales(data.sales);
       setPage(data.page);
-      setFetched(true)
+      setFetched(true);
     } catch (e) {
       console.error(e);
     } finally {
@@ -55,22 +61,48 @@ export const SaleProvider = ({ children }) => {
     }
   };
 
-  const createSale = async (product) => {
+  const createSale = async (sale) => {
+    console.log(sale);
+    setLoading(true);
     try {
-      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/products`, {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/sales`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
-        body: product,
+        body: JSON.stringify(sale),
       });
+      const data = await res.json();
+      console.log("Venta guardada:", data);
     } catch (e) {
       console.error(e);
+      throw e;
     } finally {
       setLoading(false);
     }
   };
+
+  const updateSaleStatus = async (saleId, status,ruc) => {
+    try {
+      const res = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/sales/${saleId}/status`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`, // o el que uses
+          },
+          body: JSON.stringify({ status,ruc }),
+        }
+      );
+      const data = await res.json();
+      console.log("Venta actualizada:", data);
+    } catch (error) {
+      console.error("Error al actualizar venta:", error);
+    }
+  };
+
   const updateSale = async (product) => {
     //testear si agarra sin el parametro de id
     try {
@@ -114,7 +146,17 @@ export const SaleProvider = ({ children }) => {
 
   return (
     <SaleContext.Provider
-      value={{ sales, sale, loading, getSales, getSalesById,createSale,updateSale,deleteSale }}
+      value={{
+        sales,
+        sale,
+        loading,
+        getSales,
+        getSalesById,
+        createSale,
+        updateSale,
+        updateSaleStatus,
+        deleteSale,
+      }}
     >
       {children}
     </SaleContext.Provider>
